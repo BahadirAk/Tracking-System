20f4331ff43a3ac4c3c9b9eb4b54427e104b2c84
BahadirAk <ak_bahad@hotmail.com>
2021-09-07 17:59:52 +0300

    Yorum satırı eklendi.

--- TrackingSystem/Forms/WorkForm.cs
+++ TrackingSystem/Forms/WorkForm.cs
@@ -34,7 +34,7 @@ namespace TrackingSystem.Forms
 			FillWorkListByPersonComboBox();
 			FillWorKListByTeamComboBox();
 			ResetTexts();
-		}
+		}//
 
 		private void workMenuButton_Click(object sender, EventArgs e)
 		{
